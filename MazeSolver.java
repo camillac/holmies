964b56bf6906d4de7b9a1fc4efdabcded55cc870
camillac <32921714+camillac@users.noreply.github.com>
2018-04-11 13:19:25 -0400

    Create MazeSolver.java

---
+++ MazeSolver.java
@@ -0,0 +1,10 @@
+public class MazeSolver {
+  
+  public boolean mSolver( Maze maze ) {
+    if (maze.explorerIsOnA() == maze.TREASURE)
+      return true;
+    if (maze.explorerIsOnA() == maze.WALL )
+      return false;
+    else
+  }
+  
