f05616d8c7a1471ba7c41cb46d5922eb04d6b7ac
camillac <32921714+camillac@users.noreply.github.com>
2018-04-11 20:06:44 -0400

    turned the pseudocode to code
    I didn't fully complete the work for snapshots, which I plan to discuss later.

--- MazeSolver.java
+++ MazeSolver.java
@@ -5,6 +5,23 @@ public class MazeSolver {
       return true;
     if (maze.explorerIsOnA() == maze.WALL )
       return false;
-    else
+    else {
+      snapShot; // do we print out the snapshot? or print it out later
+      for (int dir = 0; dir < 4; dir++) {
+        maze.dropA( maze.WALL );
+        go( Math.pow(2, dir) );
+        if ( mSolver(maze) )
+          return true;
+        else
+          // set current maze to snapshot; ? Not sure how or what to do
+      }
+      return false
+    }
   }
   
+  private static void snapShot( Maze candidate ) {
+    
+    Maze snapshot;
+    snapshot = new Maze(candidate);
+    
+  }
