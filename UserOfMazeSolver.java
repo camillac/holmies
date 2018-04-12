90d37a5f78f27c12b73df62fc25ee8e9cce90530
Lisa Ye <lisaye1999@gmail.com>
2018-04-11 22:42:44 -0400

    added header for UserOfMazeSolver

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -1,3 +1,16 @@
+/**
+  Test MazeSolver class.
+
+  Recycled from UserOfMaze:
+
+  Requires command line arguments:
+  o  the name of a file containing a maze.
+  o  the rank and file where an explorer is starting
+
+  For example,
+      java UserOfMaze mazes/4cell_treasureWest.txt -1 -1  # no explorer
+ */
+
 public class UserOfMazeSolver {
   
   public static void main (String[] commandLine) 
