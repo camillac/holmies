e8ebeb5ce374972629113af7184ff4b56d07e804
camillac <32921714+camillac@users.noreply.github.com>
2018-04-11 22:10:32 -0400

    added comments and removed snapShot method

--- MazeSolver.java
+++ MazeSolver.java
@@ -6,10 +6,16 @@
 public class MazeSolver {
   
   public boolean mSolver( Maze maze ) {
+    
+    // base case 0
     if (maze.explorerIsOnA() == maze.TREASURE)
       return true;
+    
+    // base case 1
     if (maze.explorerIsOnA() == maze.WALL )
       return false;
+    
+    // recursive case
     else {
       
       Maze snapshot = new Maze(maze);
@@ -17,18 +23,14 @@ public class MazeSolver {
       for (int dir = 0; dir < 4; dir++) {
         maze.dropA( maze.WALL );
         go( Math.pow(2, dir) );
-        if ( mSolver(maze) )
+        if ( mSolver(maze) ) // if the recursive abstraction returns true
           return true;
         else
           maze = snapshot;
-      }
+      } // end of for loop
       return false;
     }
+    
   }
   
-  private static void snapShot( Maze candidate ) {
-    
-    Maze snapshot;
-    snapshot = new Maze(candidate);
-    
-  }
+}
