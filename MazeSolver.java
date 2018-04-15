52f6a940b2f8b1443cbcd9c09095db83ea6e5a15
Lisa Ye <lisaye1999@gmail.com>
2018-04-15 19:39:02 -0400

    added comments

--- MazeSolver.java
+++ MazeSolver.java
@@ -14,29 +14,31 @@ public class MazeSolver {
     
     System.out.println("Explorer has moved:" + System.lineSeparator() + maze );
     
-    // base case 0
+    // base case 0: on treasure
     if (maze.explorerIsOnA() == Maze.TREASURE)
       return true;
     
-    // base case 1
+    // base case 1: on a wall
     if (maze.explorerIsOnA() == Maze.WALL )
       return false;
     
     // recursive case
     else {
-      
+      //takes a snapshot
       Maze snapshot = new Maze(maze);
       for (int dir = 0; dir < 4; dir++) {
-		
-		debug("Snapshot taken:", snapshot);
+                // debug("Snapshot taken:", snapshot);
+        //drops a wall in current position
         maze.dropA( Maze.WALL );
+        //moves toward a direction
         maze.go( directions[dir] );
         if ( mazeSolver( maze ) ) // if the recursive abstraction returns true
-          return true;
-        else
-          debug("Before restoration:" + dir, maze);
+          return true; 
+        else 
+                // debug("Before restoration:" + dir, maze);
+          //restores the maze to snapshot
           maze = new Maze(snapshot);
-          debug("After restoration:" + dir, maze);
+                // debug("After restoration:" + dir, maze);
       } // end of for loop
       return false;
     }
