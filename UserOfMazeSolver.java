7bff3f17b11786c9413ed7819975822bde7d3ee8
camillac <32921714+camillac@users.noreply.github.com>
2018-04-19 05:14:06 -0400

    try-catch for fileNotFound
    hwA0 v0

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -14,13 +14,20 @@
 public class UserOfMazeSolver {
   
   public static void main (String[] commandLine) 
-  throws java.io.FileNotFoundException {
+  try { //tests if it is possible to make maze with given strings in the command line
     Maze testMaze0 = new Maze( commandLine[0]
                              , Integer.parseInt( commandLine[1])
                              , Integer.parseInt( commandLine[2])
                              );
-    System.out.println( MazeSolver.mazeSolver( testMaze0 ) );
+  } catch (java.io.FileNotFoundException unusedID) { //if file not found, use default maze
+    Maze testMaze0 = new Maze( "4cell_treasureWest.txt"
+                              , Integer.parseInt( commandLine[1] )
+                              , Integer.parseInt( commandLine[2] )
+                              ); 
+    System.out.println( "Maze File Not Found, Will run with default maze: "
+                       + System.lineSeparator());
   }
+    System.out.println( MazeSolver.mazeSolver( testMaze0 ) );
 }
   
 // not sure if we need any more methods that were used in UserOfMaze since they were all test methods
